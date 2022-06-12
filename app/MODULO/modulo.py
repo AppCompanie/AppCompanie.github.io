75e73001a091399bce33a3039b45bbdf1b47b2ac
AppCompanie <107271684+AppCompanie@users.noreply.github.com>
2022-06-12 09:27:55 +0200

    Add files via upload

--- app/MODULO/modulo.py
+++ app/MODULO/modulo.py
@@ -4,3 +4,12 @@ Bienvenue sur le MODULO !
 
 def addition(nbr1, nbr2):
     print(float(nbr1)+float(nbr2))
+
+def soustraction(nbr1, nbr2):
+    print(float(nbr1)-float(nbr2))
+
+def division(nbr1, nbr2):
+    print(float(nbr1)/float(nbr2))
+
+def multiplication(nbr1, nbr2):
+    print(float(nbr1)*float(nbr2))
